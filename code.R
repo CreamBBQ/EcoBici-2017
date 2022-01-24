30698265ad98562515173234b6e63b57291cbc6f
CreamBBQ <jgmd1306@gmail.com>
2022-01-24 06:54:38 -0300

    Cargo los datos y computo los números de filas y columnas

--- code.R
+++ code.R
@@ -1,1 +1,14 @@
 rm(list = ls())
+setwd("/home/creambbq/bairesBikes")  
+
+#librerias 
+library("dplyr")
+library("ggplot2")
+
+#datos
+datos <-  read.table("trips_2021.csv", header = TRUE, sep = ",", stringsAsFactors = TRUE)
+
+#script
+fil <- nrow(datos)
+col <- ncol(datos)
+
