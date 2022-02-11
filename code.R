59394d198484a2b96dd924ffb632c42e4af6a0b2
CreamBBQ <jgmd1306@gmail.com>
2022-02-11 16:12:13 -0300

    Agrego los gráficos necesarios para el análisis

--- code.R
+++ code.R
@@ -26,6 +26,58 @@ datos <- datos %>% mutate(misma_estacion = case_when((bici_estacion_origen == bi
                           misma_estacion = as.factor(misma_estacion))
 
 ##Gráficos 
+#Minutos de viaje dependiendo de si es fin de semana o no. 
+ggplot(datos, 
+       aes(x=bici_tiempo_uso, fill = fin_de_semana, ..scaled..)) + 
+  geom_density(alpha = 0.66) + 
+  coord_cartesian(xlim = c(0, 200)) + 
+  labs(x="Duracion de los viajes en minutos", y = "Densidad") +
+  scale_fill_manual(values = c("#004c69","#72737e")) +
+  scale_x_continuous(breaks = seq(0,200,15)) + 
+  theme(legend.title = element_blank(), legend.position = "top")
+#sum(is.na(datos_bicis$bici_tiempo_uso) == TRUE)
+#Hora de extracción distinguiendo por si es fin de semana o no
+datos %>% group_by(fecha, fin_de_semana, hour) %>% summarise(ext = n()) %>% 
+  group_by(fin_de_semana, hour) %>% summarise(media = mean(ext)) %>% 
+  ggplot(aes(x=hour, y = media, fill = fin_de_semana)) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x="Hora del dia", y= "Extracciones promedio") +
+  scale_fill_manual(values = c("#004c69","#72737e")) +
+  scale_x_continuous(breaks = seq(0,23,2)) +
+  scale_y_continuous(breaks = seq(0,500,50)) + 
+  theme(legend.title = element_blank())
+#Figura con facets que nos permite ver la densidad de extracciones en el eje 
+#vertical, la duración del viaje en el horizontal y distingue en cuatro paneles
+#con las combinaciones posibles de la varibale "fin_de_semana" y "misma_estación"
+datos %>% group_by(bici_nombre_estacion_origen) %>% 
+  filter(n()>999, is.na(misma_estacion) == FALSE, bici_sexo!="NO INFORMADO") %>%
+  ggplot(aes(x= bici_tiempo_uso, fill = bici_sexo, ..scaled..)) + 
+  geom_density(alpha = 0.75) + 
+  coord_cartesian(xlim = c(0, 150)) + 
+  scale_fill_manual(values =c("#66c880","#eb872a"), labels = c("Mujeres", "Hombres")) +
+  labs(x="Duracion de los viajes en minutos", y = "Densidad de extracción") + 
+  facet_grid( misma_estacion ~ fin_de_semana ) +
+  theme_light() +
+  theme(legend.title = element_blank())
 
+#Evolución anual de la extracción de bicicletas distinguiendo por sexo
+datos %>% filter(bici_sexo != "NO INFORMADO") %>%  group_by(fecha, bici_sexo) %>% 
+  summarise(cant = n()) %>% 
+  ggplot(aes(x=fecha, y=cant, colour = bici_sexo)) +
+  geom_point(alpha = .7) + 
+  geom_smooth() +
+  scale_color_manual(values = c("#66c880","#eb872a"), 
+                     labels = c("Mujeres", "Hombres")) + 
+  labs(y = "Cantidad de extracciones", x = "Fecha") +
+  theme_gray() + 
+  theme(legend.title = element_blank())
+
+#20 estaciones con mayor cantidad de extracciones 
+datos %>% group_by(bici_nombre_estacion_origen) %>% summarise(cant = n()) %>% 
+  arrange(desc(cant)) %>% top_n(20) %>% 
+  ggplot(aes(x=cant, y = reorder(bici_nombre_estacion_origen, -cant))) + 
+  geom_bar(stat = "identity", fill = "#f4e04d") +
+  theme_dark() +
+  labs(x = "Cantidad de extracciones", y = " ") 
 
 
