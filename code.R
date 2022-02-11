b56c2cca5f2422c60e285d356017ed3474aebacf
CreamBBQ <jgmd1306@gmail.com>
2022-02-11 16:10:45 -0300

    Renombre de variables para ayudar a los gráficos

--- code.R
+++ code.R
@@ -16,16 +16,16 @@ datos$bici_Fecha_hora_retiro <- as.POSIXct(datos$bici_Fecha_hora_retiro, format=
 datos$fecha <- as.Date(datos$bici_Fecha_hora_retiro, tz = "America/Argentina/Buenos_Aires" )
 datos$bici_tiempo_uso_sec <- datos$bici_tiempo_uso * 60
 datos$bici_Fecha_hora_devolucion <- datos$bici_Fecha_hora_retiro + datos$bici_tiempo_uso_sec
-datos$dia <-  strftime(datoss$bici_Fecha_hora_retiro, format = "%w")
+datos$dia <-  strftime(datos$bici_Fecha_hora_retiro, format = "%w")
 datos <- datos %>% mutate(hour = strftime(bici_Fecha_hora_retiro, format = "%H"),
                           hour = as.numeric(hour))
-datos <- datos %>% mutate(fin_de_semana = case_when(dia %in% c(6,0) ~ "finde", dia %in% c(1,2,3,4,5,) ~ "nofinde"), 
-                          as.factor(fin_de_semana))
-datos <- datos %>% mutate(misma_estacion = case_when((bici_estacion_origen == bici_estacion_destino) == TRUE ~ "misma",
-                                                     (bici_estacion_origen == bici_estacion_destino) == FALSE ~ "distinta"),
+datos <- datos %>% mutate(fin_de_semana = case_when(dia %in% c(6,0) ~ "Fin de semana", dia %in% c(1,2,3,4,5) ~ "Día de semana"), 
+                          fin_de_semana = as.factor(fin_de_semana))
+datos <- datos %>% mutate(misma_estacion = case_when((bici_estacion_origen == bici_estacion_destino) == TRUE ~ "Misma estación",
+                                                     (bici_estacion_origen == bici_estacion_destino) == FALSE ~ "Distinta estación"),
                           misma_estacion = as.factor(misma_estacion))
 
-##gráficos
+##Gráficos 
 
 
 
